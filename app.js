f1b685a081279b5ba2a30ab67856f3d4c3a5b51e
ayushprajapati-growexxer <ayush.prajapati@growexx.com>
2026-02-05 16:36:34 +0530

    Add users API endpoint

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+
 const app = express();
+
 app.get('/', (req, res) => {
 res.send('Welcome to the App');
 });
 
 
+app.get('/api/users', (req, res) => {
+res.json({ users: [] });
+});
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
 });
